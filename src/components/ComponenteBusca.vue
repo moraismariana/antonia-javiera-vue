cf55edc6e7fc0e5322a1663b5f80c39c849d0aad
moraismariana <moraismariana200@gmail.com>
2025-02-12 11:34:02 -0300

    Busca e Paginação

---
+++ src/components/ComponenteBusca.vue
@@ -0,0 +1,31 @@
+<template>
+  <form>
+    <input name="busca" id="busca" type="text" v-model="busca" />
+    <input
+      type="submit"
+      id="lupa"
+      value="Buscar"
+      @click.prevent="buscarProdutos"
+    />
+  </form>
+</template>
+
+<script>
+export default {
+  name: "ComponenteBusca",
+  data() {
+    return {
+      busca: "",
+    };
+  },
+  methods: {
+    buscarProdutos() {
+      if (this.$route.query.q !== this.busca) {
+        this.$router.push({ query: { q: this.busca } });
+      }
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/views/Conteudos.vue
+++ src/views/Conteudos.vue
@@ -8,6 +8,7 @@
 
     <section class="conteudos-conteudo">
       <h1>Artigos, projetos e conteúdos</h1>
+      <componente-busca></componente-busca>
       <div v-if="artigos" class="conteudos-artigos">
         <router-link
           v-for="artigo in artigos"
@@ -25,6 +26,15 @@
           <p>{{ artigo.descricao }}</p>
         </router-link>
       </div>
+      <div v-if="apiPrevious">
+        <button @click.prevent="alterarPagina(apiPrevious)">
+          Página anterior
+        </button>
+      </div>
+      <div v-if="apiNext">
+        <button @click.prevent="alterarPagina(apiNext)">Próxima página</button>
+      </div>
+      <p>Total de artigos: {{ apiCount }}</p>
     </section>
 
     <componente-footer class="w2"></componente-footer>
@@ -36,21 +46,69 @@ import { api } from "@/axios/index.js";
 
 import ComponenteFooter from "@/components/ComponenteFooter.vue";
 import ComponenteHeader from "@/components/ComponenteHeader.vue";
+import ComponenteBusca from "@/components/ComponenteBusca.vue";
 
 export default {
   name: "Conteudos",
   components: {
     ComponenteHeader,
     ComponenteFooter,
+    ComponenteBusca,
   },
   data() {
     return {
       artigos: null,
+      apiNext: null,
+      apiPrevious: null,
+      apiCount: null,
     };
+  },
+  computed: {
+    url() {
+      let queryString = "";
+      for (let key in this.$route.query) {
+        queryString += `&${key}=${this.$route.query[key]}`;
+      }
+      return `?${queryString.slice(1)}`;
+    },
   },
   methods: {
     getArtigos() {
-      api.get("/artigos/").then((response) => (this.artigos = response.data));
+      api.get(`/artigos/${this.url}`).then((response) => {
+        if (response.data.results) {
+          this.artigos = response.data.results;
+        }
+        if (response.data.next) {
+          this.apiNext = response.data.next;
+        }
+        if (response.data.previous) {
+          this.apiPrevious = response.data.previous;
+        }
+        if (response.data.count) {
+          this.apiCount = response.data.count;
+        }
+      });
+    },
+    alterarPagina(link) {
+      api.get(link).then((response) => {
+        if (response.data.results) {
+          this.artigos = response.data.results;
+        }
+        if (response.data.next) {
+          this.apiNext = response.data.next;
+        }
+        if (response.data.previous) {
+          this.apiPrevious = response.data.previous;
+        }
+        if (response.data.count) {
+          this.apiCount = response.data.count;
+        }
+      });
+    },
+  },
+  watch: {
+    url() {
+      this.getArtigos();
     },
   },
   created() {
