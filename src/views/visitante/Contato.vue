929c3d7a6362c0f10298e01a78ce57a240002d4e
moraismariana <moraismariana200@gmail.com>
2025-02-17 15:10:31 -0300

    Requisições Formulário de Contato

--- src/views/visitante/Contato.vue
+++ src/views/visitante/Contato.vue
@@ -10,18 +10,36 @@
         <div>
           <div class="contato-form-element">
             <label for="nome">Nome</label>
-            <input type="text" id="nome" name="nome" />
+            <input
+              v-model="formularioContato.nome"
+              type="text"
+              id="nome"
+              name="nome"
+              required
+            />
           </div>
           <div class="contato-form-element">
             <label for="email">Email</label>
-            <input type="email" id="email" name="email" />
+            <input
+              v-model="formularioContato.email"
+              type="email"
+              id="email"
+              name="email"
+              required
+            />
           </div>
         </div>
         <div class="contato-form-element">
           <label for="mensagem">Mensagem</label>
-          <textarea name="mensagem" id="mensagem" rows="5"></textarea>
+          <textarea
+            v-model="formularioContato.mensagem"
+            name="mensagem"
+            id="mensagem"
+            rows="5"
+            required
+          ></textarea>
         </div>
-        <button>Enviar</button>
+        <button @click.prevent="enviarEmail">Enviar</button>
       </form>
     </section>
 
@@ -30,6 +48,7 @@
 </template>
 
 <script>
+import { api } from "@/axios/index.js";
 import ComponenteFooter from "@/components/ComponenteFooter.vue";
 import ComponenteHeader from "@/components/ComponenteHeader.vue";
 
@@ -38,6 +57,50 @@ export default {
   components: {
     ComponenteHeader,
     ComponenteFooter,
+  },
+  data() {
+    return {
+      formularioContato: {
+        nome: "",
+        email: "",
+        mensagem: "",
+      },
+    };
+  },
+  methods: {
+    enviarEmail() {
+      api
+        .post("/contato/", this.formularioContato)
+        .then((response) => {
+          console.log(response);
+          console.log(response.data);
+          if (response.status === 200) {
+            window.alert("Email enviado com sucesso.");
+            this.formularioContato.nome = "";
+            this.formularioContato.email = "";
+            this.formularioContato.mensagem = "";
+          }
+        })
+        .catch((erro) => {
+          console.log(erro);
+          switch (erro.status) {
+            case 400:
+              if (erro.response.data.nome) {
+                window.alert(`${erro.response.data.nome[0]}`);
+              } else if (erro.response.data.email) {
+                window.alert(`${erro.response.data.email[0]}`);
+              } else if (erro.response.data.mensagem) {
+                window.alert(`${erro.response.data.mensagem[0]}`);
+              }
+              break;
+            case 500:
+              window.alert(
+                "Não foi possível enviar o email no momento. Tente novamente mais tarde."
+              );
+              break;
+          }
+        });
+    },
   },
 };
 </script>
